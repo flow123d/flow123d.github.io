833f4191a9c3aec38d004f5f5796106505edc7ae
Jiri Hnidek <jiri.hnidek@tul.cz>
2014-04-28 14:26:37 +0200

    Minor and major fixes.

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,8 +4,8 @@
 $(document).ready(function() {
   var version = $("#version_selector").val() || "1.8.0";
 
-  var root_url = "http://flow123d.github.io/";
-  //var root_url = "file:///home/jiri/Flow/flow123d.github.io/";
+  //var root_url = "http://flow123d.github.io/";
+  var root_url = "file:///home/jiri/Flow/flow123d.github.io/";
 
   $("#flow_version").html(version);
 
@@ -34,82 +34,24 @@ $(document).ready(function() {
                     <option value="1.8.0" selected="selected">Release - 1.8.0</option>\
                     <option value="1.7.0">Release - 1.7.0</option>\
                     <option value="1.6.0">Release - 1.6.0</option>\
+                    <option value="night_builds">Night Builds</option>\
                   </select>\
                 </span>\
               </li>');
 
   $("#il_inner #dynamic_menu").append('<li id="features">\
     <a id="features_link" href="'+ root_url + version + '/features">Features</a></li>');
-  $("#il_inner #dynamic_menu").append('<li id="documentation">\
-    <a id="doc_link" href="'+ root_url + version + '/documentation.html">Documentation</a></li>');
   $("#il_inner #dynamic_menu").append('<li id="download">\
     <a id="download_link" href="'+ root_url + version + '/download.html">Download</a></li>');
   $("#il_inner #dynamic_menu").append('<li id="changes">\
     <a id="changes_link" href="'+ root_url + version + '/changes.html">Changes</a></li>');
   $("#il_inner #dynamic_menu").append('<li id="ref_manual">\
-    <a id="ref_manual_link" href="'+ root_url + version + '/ref_manual.html">Reference Manual</a></li>');
+    <a id="ref_manual_link" href="'+ root_url + version + '/user_manual.html">User Manual</a></li>');
   $("#il_inner #dynamic_menu").append('<li id="source_doc">\
     <a id="source_doc_link" href="'+ root_url + version + '/source_doc.html">Source Documentation</a></li>');
   $("#il_inner #dynamic_menu").append('<li id="readme">\
     <a id="readme_link" href="'+ root_url + version + '/readme.html">ReadMe</a></li>');
 
-  /* Callback function called, when user moves back or forward in history */
-/* It doesn't work corectly and it doesn't work in old browsers
-  window.onpopstate = function(event) {
-    if(event.state) {
-      var str = root_url + event.state.url + "#main_content > *";
-      $("#main_content").load(str);
-    }
-  }
-*/
-
-  /* Load main page using AJAX */
-  $('#home_link').click(function() {
-    $("#main_content").load(root_url + "index.html #main_content > *");
-    /*
-    if(window.history && window.history.pushState) {
-      var stateObj = { url: "index.html" };
-      window.history.pushState(stateObj, "Flow123d", root_url + "index.html");
-    }
-    */
-    return false;
-  });
-
-  /* Load license page using AJAX */
-  $('#license_link').click(function() {
-    $("#main_content").load(root_url + "license.html #main_content > *");
-    /*
-    if(window.history && window.history.pushState) {
-      var stateObj = { url: "license.html" };
-      window.history.pushState(stateObj, "Flow123d - License", root_url + "license.html");
-    }
-    */
-    return false;
-  });
-
-  /* Load acknowledgement page using AJAX */
-  $('#ack_link').click(function() {
-    $("#main_content").load(root_url + "acknowledgment.html #main_content > *");
-    /*
-    if(window.history && window.history.pushState) {
-      var stateObj = { url: "acknowledgment.html" };
-      window.history.pushState(stateObj, "Flow123d - Acknowledgments", root_url + "acknowledgment.html");
-    }
-    */
-    return false;
-  });
-
-  /* Load contact page using AJAX */
-  $('#contact_link').click(function() {
-    $("#main_content").load("contact.html #main_content > *");
-    /*
-    if(window.history && window.history.pushState) {
-      var stateObj = { url: "contact.html" };
-      window.history.pushState(stateObj, "Flow123d - Contact", root_url + "contact.html");
-    }
-    */
-    return false;
-  });
 });
 
 /**
@@ -120,8 +62,8 @@ function changeItems()
   var duration = 200;
   var version = $("#version_selector").val();
 
-  var root_url = "http://flow123d.github.io/";
-  //var root_url = "file:///home/jiri/Flow/flow123d.github.io/";
+  //var root_url = "http://flow123d.github.io/";
+  var root_url = "file:///home/jiri/Flow/flow123d.github.io/";
 
   /* TODO: Change content of current page too */
 
@@ -135,11 +77,6 @@ function changeItems()
     $(this).fadeIn(duration);
   });
 
-  $("#il_inner #dynamic_menu #documentation").fadeOut(duration, function() {
-    $(this).html('<a id="doc_link" href="' + root_url + version + '/documentation.html">Documentation</a>');
-    $(this).fadeIn(duration+100);
-  });
-
   $("#il_inner #dynamic_menu #download").fadeOut(duration, function() {
     $(this).html('<a id="download_link" href="' + root_url + version + '/download.html">Download</a>');
     $(this).fadeIn(duration+200);
@@ -151,7 +88,7 @@ function changeItems()
   });
 
   $("#il_inner #dynamic_menu #ref_manual").fadeOut(duration, function() {
-    $(this).html('<a id="ref_manual_link" href="' + root_url + version + '/ref_manual.html">Reference Manual</a>');
+    $(this).html('<a id="ref_manual_link" href="' + root_url + version + '/user_manual.html">User Manual</a>');
     $(this).fadeIn(duration+400);
   });
 
